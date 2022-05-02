b1aef382b1ea83e5f6236abbd7a9d2cdce1ea5d2
Ethan Rahimian <nicootech@gmail.com>
2022-05-02 16:57:38 -0700

    start point

---
+++ app/src/main/java/com/nicootech/rabinhoodconstraintlayout/ImageActivity.kt
@@ -0,0 +1,35 @@
+package com.nicootech.rabinhoodconstraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.transition.TransitionManager
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintSet
+import kotlinx.android.synthetic.main.activity_image.*
+
+
+class ImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image)
+        addAnimationOperations()
+    }
+
+    private fun addAnimationOperations() {
+        var set = false
+        val constraint1 = ConstraintSet()
+        constraint1.clone(mainroot)
+        val constraint2 = ConstraintSet()
+        constraint2.clone(this, R.layout.activity_image_alt)
+
+
+        findViewById<ImageView>(R.id.imageView).setOnClickListener{
+                TransitionManager.beginDelayedTransition(mainroot)
+                val constraint = if(set) constraint1 else constraint2
+                constraint.applyTo(mainroot)
+                set = !set
+        }
+
+    }
+}
+
